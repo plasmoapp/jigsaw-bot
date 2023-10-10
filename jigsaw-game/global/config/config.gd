44cf49389c88d3b27c3b2b93e5c6501b10afad5c
KPidS <WKPidS@gmail.com>
2023-10-10 13:16:30 +0300

    Attemp to fix UI scaling issue

--- jigsaw-game/global/config/config.gd
+++ jigsaw-game/global/config/config.gd
@@ -3,6 +3,8 @@ extends Node
 # Used in the editor
 export var test_url: String
 
+export var default_ui_scaling: int = 1
+
 onready var base_url = "http://%s" % test_url
 onready var base_ws_url = "ws://%s" % test_url
 
@@ -11,3 +13,10 @@ func _ready():
 		var host = JavaScript.eval("window.location.host")
 		base_url = "https://%s" % host
 		base_ws_url = "wss://%s" % host
+		
+	print(OS.get_screen_dpi())
+		
+	var viewport: Viewport = get_tree().get_root()
+	
+	# get_viewport().size_override_stretch()
+
